fc375b49c0a14999b218e97cd27be24ccdc6cd98
ImagineGavins <imaginegavins@gmail.com>
2022-03-02 14:58:19 -0500

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express') // const dont change, let can change
+const app = express()
+const path = require('path')
+const port = process.env.port||3000
+
+app.use(express.static(__dirname+"/views"))
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname+"/views/index.html")
+})
+
+// app.listen sets up a connection
+app.listen(port, ()=>{
+    console.log(`Connected to port ${port}`)
+})
